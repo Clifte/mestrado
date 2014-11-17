0cf5f9a954772346b32fc87d39427318c73fc343
clifte <clifte@clifte-desktop>
2014-11-17 18:00:36 -0300

    criação da rede SON

--- Matlab/Trabalho RNA/trabalho2/SON/run.m
+++ Matlab/Trabalho RNA/trabalho2/SON/run.m
@@ -13,16 +13,23 @@ pTeste = 0.2;
  
 %Carregando dados
 [ x , y ] = carregaDatabase('iris');
+[m n] = size(x);
 
 %Normalizando X.
-[m n] =size(x);
-mx = mean(x);
-sx = std(x);
-nx = (x - repmat(mx,m,1)) ./ repmat(sx,m,1);
+maxx = max(x);
+maxx = repmat(maxx,[m 1]);
+
+minx = min(x);
+minx = repmat(minx,[m 1]);
+
+nx = x - minx;
+d = maxx - minx;
+nx = nx ./d;
+
 
 %%
 
-net = som(x(:,1:2) , 25);
+net = som(nx(:,1:2) , 25);
 
 
 
